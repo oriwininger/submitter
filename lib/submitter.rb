3dd4dc3e24325427d1c28292104cf2fddb1c3a3d
Ori Wwininger <oriwininger0@gmail.com>
2013-07-29 15:29:35 +0300

    fixed module bug

--- lib/submitter.rb
+++ lib/submitter.rb
@@ -1,12 +1,4 @@
 require "submitter/version"
 
 module Submitter
-	module models
-  # Your code goes here...
-		class job_application(:application)
-			def new
-				JobApplication.new
-			end
-		end
-	end
 end
--- lib/submitter/version.rb
+++ lib/submitter/version.rb
@@ -1,3 +1,3 @@
 module Submitter
-  VERSION = "0.0.1"
+  VERSION = "0.0.2"
 end
